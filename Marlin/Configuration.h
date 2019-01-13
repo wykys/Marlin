a53b863679c60203e30c252692c6db73afb37b8c
wykys <vvykys@gmail.com>
2019-01-14 00:28:35 +0100

    Updated Z_HOME_POS

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1142,7 +1142,7 @@
 // For DELTA this is the top-center of the Cartesian print volume.
 #define MANUAL_X_HOME_POS -15
 #define MANUAL_Y_HOME_POS -20
-#define MANUAL_Z_HOME_POS 0.9
+#define MANUAL_Z_HOME_POS 1.2
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
 //
