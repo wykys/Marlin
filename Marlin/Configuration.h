b7c0f23723fa218cac57b02daaf7657eab4ada12
Jan Vykydal <john.vykydal@gmail.com>
2020-01-12 01:55:43 +0100

    Fixed display timing problem

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1669,7 +1669,7 @@
 // http://reprap.org/wiki/RepRapDiscount_Full_Graphic_Smart_Controller
 //
 #define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
-
+#define ST7920_DELAY_1 DELAY_NS(200)
 //
 // ReprapWorld Graphical LCD
 // https://reprapworld.com/?products_details&products_id/1218
